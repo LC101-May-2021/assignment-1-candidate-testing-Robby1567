072d71f728c64db90f5a3b6ab1680e8ae4b49c00
Robby Ross <>
2021-05-24 22:52:04 +0000

    let candidate name be defined as empty space, used candidateName = input.question to ask for for the human to input name, and finally added a console.log to make the program greet the user.

--- candidate-testing.js
+++ candidate-testing.js
@@ -15,7 +15,7 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-
+candidateName = input.question("candinate name:  ")
 }
 
 function askQuestion() {
@@ -38,7 +38,7 @@ function gradeQuiz(candidateAnswers) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
-  
+  console.log("Hello "+candidateName+"!");
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
